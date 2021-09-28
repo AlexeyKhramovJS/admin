e56d06b886fa1959e6f45b493b573b8f51ffc726
Alexey Khramov <Khramov.header@yandex.ru>
2021-09-29 00:21:50 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,273 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- <div>
+      {{ users }}
+    </div> -->
+      <v-data-table
+    :headers="headers"
+    :items="users"
+    sort-by="id"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Users</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              New Item
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.id"
+                      label="ID"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.surname"
+                      label="Surname"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.email"
+                      label="E-mail"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.status"
+                      label="Status"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this user?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Name', value: 'name' },
+        { text: 'Surname', value: 'surname' },
+        { text: 'E-mail', value: 'email' },
+        { text: 'Status', value: 'status' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      //users: [],
+      editedIndex: -1,
+      editedItem: {
+        id: '',
+        name: '',
+        surname: 0,
+        email: 0,
+        status: 0,
+      },
+      defaultItem: {
+        id: '',
+        name: '',
+        surname: 0,
+        email: 0,
+        status: 0,
+      },
+    }),
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    computed: {
+      users() {
+        return this.$store.state.users;
+      },
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created() {
+      this.initDb();      
+    },
+    // mounted() {
+    //   this.setUsers();
+    // },
+
+    methods: {
+      initDb() {
+        this.$store.dispatch('initDb');
+      },
+      setUsers() {
+        this.$store.dispatch('getUsers');
+      },
+      editItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        console.log(item);
+        this.$store.dispatch('removeUsers', item.id);
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.users.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+        } else {
+          //this.users.push(this.editedItem)
+          this.$store.dispatch('addUser', this.editedItem);
+          this.$store.dispatch('getUsers');
+        }
+        this.close()
+      },
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,120 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [],
+    db: null
   },
   mutations: {
+    setUser(state, user) {
+    console.log('mutations');    
+      // state.users.push(user);
+      state.users = user;
+    },
+    setDb(state, link) {
+      state.db = link;
+    },
+    createUsersStore(state) {
+      state.db.createObjectStore('users', {keyPath: 'id'});
+    }
   },
   actions: {
+    initDb({dispatch, commit}) {
+      //let db;
+      let dbReq = indexedDB.open('DB', 1);
+      dbReq.onupgradeneeded = (event) => {
+        // Зададим переменной db ссылку на базу данных
+        //db = event.target.result;
+        dispatch('setLink', event.target.result);
+        // Создадим хранилище объектов с именем user.
+        //let user = db.createObjectStore('user', {autoIncrement: true});
+        dispatch('createStore');
+      }
+      dbReq.onsuccess = (event) => {
+        // db = event.target.result;
+        dispatch('setLink', event.target.result);
+        dispatch('getUsers');
+      }
+      dbReq.onerror = (event) => {
+        alert('error opening database ' + event.target.errorCode);
+      }
+    },
+    setLink({commit}, link) {
+      commit('setDb', link);
+    },
+    createStore({commit}) {
+      commit('createUsersStore');
+    },
+    addUser({commit, state, dispatch}, user) {
+      let tx = state.db.transaction(['users'], 'readwrite');
+      let store = tx.objectStore('users');
+      // Добаляем user в хранилище объектов
+      store.add(user);
+      // Ожидаем завершения транзакции базы данных
+      tx.oncomplete = () => {
+        console.log('stored note!');
+      }
+      tx.onerror = (event) => {
+        alert('error storing note ' + event.target.errorCode);
+      }
+    },
+    getUsers({commit, state}) {
+      let tx = state.db.transaction(['users'], 'readonly');
+      let store = tx.objectStore('users');
+      // Создать запрос курсора
+      let req = store.openCursor();
+      let users = [];
+      req.onsuccess = (event) => {
+        // Результатом req.onsuccess в запросах openCursor является
+        // IDBCursor
+        let cursor = event.target.result;
+        if (cursor != null) {
+          // Если курсор не нулевой, мы получили элемент.
+          users.push(cursor.value);
+          console.log('push');          
+          //commit('setUser', cursor.value)
+          cursor.continue();
+        } else {
+          commit('setUser', users);
+        }
+      }
+      req.onerror = (event) => {
+        alert('error in cursor request ' + event.target.errorCode);
+      }
+    },
+    removeUsers({state}, id) {
+      console.log(typeof id);
+      
+      // открываем транзакцию чтения/записи БД, готовую к удалению данных
+      const tx = state.db.transaction(['users'], 'readwrite');
+      // описываем обработчики на завершение транзакции
+      tx.oncomplete = (event) => {
+        console.log('Transaction completed.')
+        //getAndDisplayNotes(db);
+      };
+      tx.onerror = function(event) {
+        alert('error in cursor request ' + event.target.errorCode);
+      };
+      // создаем хранилище объектов по транзакции
+      const store = tx.objectStore('users');
+      console.dir(store);
+      const index = store.index(id);
+       // получаем ключ записи
+      const req = index.getKey(id)
+      req.onsuccess = (event) => {  
+        const key = req.result;
+        // выполняем запрос на удаление указанной записи из хранилища объектов
+        let deleteRequest = store.delete(key);
+        deleteRequest.onsuccess = (event) => {
+          // обрабатываем успех нашего запроса на удаление
+          console.log('Delete request successful')
+        };    
+      }
+
+    }
   },
-  modules: {
+  getters: {
+    users(state) {
+      return state.users;
+    }
   }
 })
